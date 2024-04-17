13dd37321caa4d4ba68050dc84233391f714ce9b
PadmnabhSrivastava <padmanabhsrivastava@gmail.com>
2024-04-17 16:13:38 +0530

    new features

--- src/main/java/com/exam/ExamserverApplication.java
+++ src/main/java/com/exam/ExamserverApplication.java
@@ -1,61 +1,98 @@
 package com.exam;
 
+import com.exam.helper.UserFoundException;
 import com.exam.model.Role;
 import com.exam.model.User;
 import com.exam.model.UserRole;
+//import com.exam.model.exam.Quiz;
+//import com.exam.repo.QuizRepository;
 import com.exam.service.UserService;
-
-import java.util.HashSet;
-import java.util.Set;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @SpringBootApplication
-public class ExamserverApplication implements CommandLineRunner{
-	
-	@Autowired
-	private UserService userService;
+public class ExamserverApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ExamserverApplication.class, args);
-		
-	}
+    @Autowired
+    private UserService userService;
 
-	@SuppressWarnings("static-access")
-	@Override
-	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		//throw new UnsupportedOperationException("Unimplemented method 'run'");
-		System.out.println("Starting Code");
+    //@Autowired
+   // private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-		//  User user = new User();
+	public PasswordEncoder passwordEncoder() {
+        //return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
+    }
 
-		//  user.setFirstName("Padmnabh");
-		//  user.setLastName("Srivastava");
-		//  user.setUsername("paddy03");
-		//  user.setPassword("abc");
-		//  user.setEmail("abc@gmail.com");
-		//  user.setProfile("default.png");
+    // @Autowired
+    // public QuizRepository quizRepository;
 
-		//  Role role1 = new Role();
-		//  role1.setRoleId(44L);
-		//  role1.setRoleName("ADMIN");
-
-		//  Set<UserRole> userRoleSet = new HashSet<>();
-		//  UserRole userRole = new UserRole();
-
-		//  userRole.setRole(role1);
-		//  userRole.setUser(user);
-
-		//  userRoleSet.add(userRole);
-
-		//  User user1 = this.userService.createUser(user, userRoleSet);
-		//  System.out.println(user1.getUsername());
+    public static void main(String[] args) {
 
 
-	}
+        SpringApplication.run(ExamserverApplication.class, args);
+
+
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+       // try {
+
+
+            System.out.println("starting code");
+//
+        //     User user = new User();
+
+        //     user.setFirstName("Padmnabh");
+        //     user.setLastName("Srivastava");
+        //     user.setUsername("Paddy03");
+        //     user.setPassword(this.passwordEncoder().encode("abc"));
+        //     user.setEmail("abc@gmail.com");
+        //     user.setProfile("default.png");
+
+        //     Role role1 = new Role();
+        //     role1.setRoleId(44L);
+        //     role1.setRoleName("ADMIN");
+
+        //     Set<UserRole> userRoleSet = new HashSet<>();
+        //     UserRole userRole = new UserRole();
+
+        //     userRole.setRole(role1);
+
+        //     userRole.setUser(user);
+
+        //     userRoleSet.add(userRole);
+
+        //     User user1 = this.userService.createUser(user, userRoleSet);
+        //     System.out.println(user1.getUsername());
+
+
+        // } catch (UserFoundException e) {
+        //     e.printStackTrace();
+
+
+        // }
+
+
+    }
+
 
 }
